4a57733c69d8e4cdbcfaf6c91997e29fb9349a69
brenobcsilva <breno.csilva@hotmail.com>
2022-08-29 16:01:34 -0300

    commit inicial

---
+++ src/main/java/com/inn/cafe/POJO/User.java
@@ -0,0 +1,64 @@
+package com.inn.cafe.POJO;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contactNumber")
+    private String contactNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+
+    public User(){
+
+    }
+
+    public User(String name, String contactNumber, String email, String password, String status, String role) {
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+        this.role = role;
+    }
+
+    public User(Integer id, String name, String contactNumber, String email, String password, String status, String role) {
+        this.id = id;
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+        this.role = role;
+    }
+}
